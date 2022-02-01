bce03decb2e9ce59a9a0350ee0136f0c9878f6ba
RawanHelmy <rawanhelmy98@gmail.com>
2022-02-01 23:50:40 +0300

    dashboard data added

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,16 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
+import { Ads } from '../../models/ads.model';
 import { DashboardService } from '../../serivces/dashboard.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class dashboardComponent implements OnInit {
+export class dashboardComponent implements OnInit, AfterViewInit {
+  selection = new SelectionModel<any>(true, []);
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
+  dataKeys: string[] = [];
+  tableData: Ads[] = [];
+  @ViewChild(MatPaginator, { static: false })
+  paginator!: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort = new MatSort();
+  @ViewChild(MatPaginator, { static: false }) set matPaginator(
+    mp: MatPaginator
+  ) {
+    this.paginator = mp;
+    this.setDataSourceAttributes();
+  }
   constructor(private dashboardService: DashboardService) {}
-
   ngOnInit(): void {
-    this.dashboardService.getAll().subscribe((res) => console.log(res));
+    this.dashboardService.getAll().subscribe((res) => {
+      this.dataSource = new MatTableDataSource(res.tableData);
+      this.dataKeys = res.dataKeys;
+    });
+  }
+  ngAfterViewInit(): void {
+    this.setDataSourceAttributes();
+  }
+  setDataSourceAttributes() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sortingDataAccessor = this.dashboardService.sortFunction();
+    this.dataSource.sort = this.sort;
+  }
+  delete(event: Event, row: Ads) {}
+  view(event: Event, row: Ads) {}
+  checkboxLabel(row?: any): string {
+    return !row
+      ? `${this.isAllSelected() ? 'select' : 'deselect'} all`
+      : `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+          row.id + 1
+        }`;
+  }
+  isAllSelected() {
+    return this.selection.selected.length === this.dataSource.data.length;
+  }
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
   }
 }
