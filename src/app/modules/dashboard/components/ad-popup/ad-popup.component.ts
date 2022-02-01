9513a903594150137a68939b35fd61aa40b083b6
RawanHelmy <rawanhelmy98@gmail.com>
2022-02-01 23:51:02 +0300

    ads popup edit/create added

---
+++ src/app/modules/dashboard/components/ad-popup/ad-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-popup',
+  templateUrl: './ad-popup.component.html',
+  styleUrls: ['./ad-popup.component.scss']
+})
+export class AdPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { dashboardComponent } from './components/dashboard/dashboard.component';
 import { dashboardRoutingModule } from './dashboard-routing.module';
+import { AdPopupComponent } from './components/ad-popup/ad-popup.component';
 
 @NgModule({
-  declarations: [dashboardComponent],
+  declarations: [dashboardComponent, AdPopupComponent],
   imports: [SharedModule, dashboardRoutingModule],
 })
 export class dashboardModule {}
